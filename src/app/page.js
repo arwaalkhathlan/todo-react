4d13297fb8ff51e3033d9f2b43aea307ecb686e1
arwaalkhathlan <arwa124344@gmail.com>
2024-07-07 08:32:07 +0300

    deleted unnecessary pages and updated the css

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,31 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Form from "@/app/components/Form";
 import Header from "@/app/components/Header";
 import TODOHero from "./components/TODOHero";
 import TODOList from "./components/TODOList";
 
 export default function Home() {
-  return (
-      <div className="wrapper">
-        <Header/>
-        <TODOHero todos_completed={0} total_todos={0}/>
-        <Form/>
-        <TODOList todos={[]}/>
-        
-        
-      </div>
+  const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    setTodos([
+      { title: "Some task", id: crypto.randomUUID(), is_completed: false },
+      {
+        title: "Some other task",
+        id: crypto.randomUUID(),
+        is_completed: true,
+      },
+      { title: "last task", id: crypto.randomUUID(), is_completed: false },
+    ]);
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <TODOHero todos_completed={0} total_todos={0} />
+      <Form />
+      <TODOList todos={todos} />
+    </div>
   );
 }
-
